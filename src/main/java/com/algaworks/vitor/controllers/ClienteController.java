20620247ebc538a4ff17c54c112ecb09347e99c7
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-09-16 10:37:29 -0300

    Usando Jakarta Persistence JPA

--- src/main/java/com/algaworks/vitor/controllers/ClienteController.java
+++ src/main/java/com/algaworks/vitor/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.vitor.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,11 @@ import com.algaworks.vitor.models.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar(){
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("vitor");
-		cliente1.setTelefone("4235345-45");
-		cliente1.setEmail("jksdhfkojdshklfgsd@gmail.com");
-		
-		return Arrays.asList(cliente1);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
--- src/main/java/com/algaworks/vitor/models/Cliente.java
+++ src/main/java/com/algaworks/vitor/models/Cliente.java
@@ -1,23 +1,33 @@
 package com.algaworks.vitor.models;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String nome;
-	
+
 	private String email;
-	
+
+	@Column(name = "fone")
 	private String telefone;
-	
-	
+
+
 }

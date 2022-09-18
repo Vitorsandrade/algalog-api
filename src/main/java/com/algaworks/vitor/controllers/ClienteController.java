c17705a3c2a8652b1fd7b9cc3d87a91499589aa0
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-09-18 15:00:15 -0300

    Validando com Bean Validation

--- src/main/java/com/algaworks/vitor/controllers/ClienteController.java
+++ src/main/java/com/algaworks/vitor/controllers/ClienteController.java
@@ -2,6 +2,8 @@ package com.algaworks.vitor.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -53,12 +55,12 @@ public class ClienteController {
 	@PostMapping
 	// serve para passar a repsosta de status 201 Created
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,@Valid @RequestBody Cliente cliente){
 		 if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
